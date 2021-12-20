e27405c5fe6787a1f6c936b29b75855f56d2e493
Bobby George <slowtacocar@gmail.com>
2021-12-20 09:29:45 -0500

    Update auth.js

--- api/pusher/auth.js
+++ api/pusher/auth.js
@@ -15,6 +15,7 @@ export default function handler(req, res) {
     if (err) {
       throw err;
     } else {
+      console.log(req.body);
       const auth = pusher.authenticate(
         req.body.socket_id,
         req.body.channel_name,
