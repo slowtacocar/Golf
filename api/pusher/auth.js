054903fab49fe4840204b2478d4041daf5259f41
Bobby George <slowtacocar@gmail.com>
2022-04-19 22:56:52 -0400

    Update auth.js

--- api/pusher/auth.js
+++ api/pusher/auth.js
@@ -17,11 +17,13 @@ export default function handler(req, res) {
     } else {
       console.log(req.body);
       const { id, ...info } = req.body.info;
+      const uid = crypto.randomUUID();
+      console.log(uid);
       const auth = pusher.authenticate(
         req.body.socket_id,
         req.body.channel_name,
         {
-          user_id: crypto.randomUUID(),
+          user_id: uid,
           user_info: info,
         }
       );
